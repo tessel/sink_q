ed5f86a4003a2138d1759493bd2970d7082e1572
Nathan Lintz <nlintz@gmail.com>
2014-06-10 16:58:55 -0700

    first commit!

---
+++ index.js
@@ -0,0 +1,48 @@
+var Queue = function (queueReady) {
+  var queue = [];
+  var queueReady = queueReady | false;
+  var self = this;
+
+  this.push = function (fn, callback) {
+    queue.push({fn:fn, callback:callback});
+
+    if (queueReady) {
+      pop();
+    };
+  };
+
+  // called on 'ready' to fire events stored before camera is ready
+  this.ready = function () {
+    unlockQueue();
+    pop();
+  }
+
+  var pop = function () {
+    if (queue.length == 0) {
+      return;
+    }
+    lockQueue();
+    var obj = queue.shift();
+    var fn = obj.fn;
+    var callback = obj.callback;
+    if (callback == undefined) {
+      callback = function() {};
+    }
+    fn(decorateCallback(callback, self.ready)); // Call Ready
+  };
+
+  var unlockQueue = function () {
+    queueReady = true;
+  }
+
+  var lockQueue = function () {
+    queueReady = false;
+  }
+}
+
+function decorateCallback (callback, decorator) {
+  return function () {
+    callback.apply(callback, arguments);
+    decorator();
+  }
+}
